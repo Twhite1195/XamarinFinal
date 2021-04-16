7fbecf4a0e01d0bf494f473586858bc157652ce4
AlfaroDan <44320570+AlfaroDanielV@users.noreply.github.com>
2021-04-15 18:49:09 -0600

    splash

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.Android/MainActivity.cs
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ProyectoFinal.Droid
 {
-    [Activity(Label = "ProyectoFinal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ProyectoFinal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
